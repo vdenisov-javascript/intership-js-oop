c29cb8b327a6a34c75173994d591e48a7dfe7041
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-01-17 11:18:35 +0300

    code for "product-movement" is empty, test failed

---
+++ tests/product-movement.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+
+
+describe('Testing "product-movement" from "Javacript-OOP"', () => {
+
+  const productMovement = require('../tasks/product-movement');
+
+  it('I can`t imagine test for it', () => {
+    const createdTable = {};
+    const myResults = { reason: 'because all data will be generated randomly' };
+
+    assert.deepEqual(createdTable, myResults);
+  });
+
+});
