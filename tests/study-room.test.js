9633d5120c9902036f6520f963eccb2d2c272592
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-01-31 19:27:51 +0300

    test for sdudy-room

--- tests/study-room.test.js
+++ tests/study-room.test.js
@@ -1,15 +1,27 @@
-const assert = require('assert');
+const expect = require('chai').expect;
 
 
 describe('Testing "study-room" from "Javacript-OOP"', () => {
 
-  const studyRoom = require('../tasks/study-room');
+  const studyRoom = require('../tasks/study-room/run');
   const courseInfo = require('./courseInfo.json');
 
-  it('should return correct object', () => {
-    const createdCourseJS = studyRoom.createCourse(courseInfo);
+  const createdCourseJS = studyRoom.createCourse(courseInfo);
+  // assert.deepEqual(createdCourseJS, courseInfo);
 
-    assert.deepEqual(createdCourseJS, courseInfo);
+  it('should have deep "string" property "name"', () => {  
+    expect(createdCourseJS)
+      .to.have.deep.property('name')
+      .to.be.a('string');
+  });
+
+  it('should have deep "array" property "themes"', () => {  
+    expect(createdCourseJS)
+      .to.have.deep.property('themes')
+      // .to.be.an('array')
+      // .that.includes(2);
+
+    // expect(createdCourseJS.name).to.be.an('array');
   });
 
 });
