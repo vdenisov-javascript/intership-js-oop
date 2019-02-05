670840ae33f14f02718bdbdd43deb1e56fbb5355
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-02-05 10:13:24 +0300

    update class employee

--- tasks/company-model/classes/employee.model.ts
+++ tasks/company-model/classes/employee.model.ts
@@ -1,40 +1,52 @@
 import { Project } from './';
 
 
+enum EmployeeStatus {
+  Free = 'free',
+  Busy = 'busy',
+}
+
+
 export class Employee {
 
   private static _lastId = 0;
 
-  public _id: number;
+  private _id:              number;
+  private _status:          string;
+  private _skills:          number;
+  private _relaxDays:       number;
+  private _currentProject:  Project;
 
-  public status: string;
-  public currentProject: Project;
-
-  public skills: number;
-  public relaxDays: number;
+  get id():             number  { return this._id; }
+  get status():         string  { return this._status; }
+  get skills():         number  { return this._skills; }
+  get relaxDays():      number  { return this._relaxDays; }
+  get currentProject(): Project { return this._currentProject; }
 
   constructor(
     public speciality: string,
   ) {
-    this._id = ++Employee._lastId;
-
-    this.status = 'free';
-    this.currentProject = null;
-    
-    this.skills = 0;
-    this.relaxDays = 0;
+    this._id              = ++Employee._lastId;
+    this._status          = EmployeeStatus.Free;
+    this._skills          = 0;
+    this._relaxDays       = 0;
+    this._currentProject  = null;   
   }
 
   public takeProject(project: Project): void {
-    this.status = 'busy';
-    this.currentProject = project;
-    this.relaxDays = 0;
+    this._status          = EmployeeStatus.Busy;
+    this._relaxDays       = 0;
+    this._currentProject  = project;
   }
 
   public leaveProject(): void {
-    this.status = 'free';
-    this.currentProject = null;
-    this.skills += 1;
+    this._status          = EmployeeStatus.Free;
+    this._skills          += 1;
+    this._currentProject  = null;
+  }
+
+  public addRelaxDay() {
+    this._relaxDays += 1;
   }
 
 }
