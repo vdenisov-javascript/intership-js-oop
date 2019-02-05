f4e4abc9eaa131bc0557cebe1a990eea3a907141
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-02-05 09:53:17 +0300

    update class project

--- tasks/company-model/classes/project.model.ts
+++ tasks/company-model/classes/project.model.ts
@@ -17,39 +17,32 @@ export class Project {
     level: [ 1, 2, 3 ],
   };
 
-  private _id: number;
-  get id(): number    { return this._id; }
-  set id(arg: number) { this._id = arg;  }
-
-  private _type: string;
-  get type(): string    { return this._type; }
-  set type(arg: string) { this._type = arg;  }
-
-  private _level: number;
-  get level(): number    { return this._level; }
-  set level(arg: number) { this._level = arg;  }
-
-  private _status: string;
-  get status(): string    { return this._status; }
-  set status(arg: string) { this._status = arg;  }
-
-  private _deadline: number;
+  private _id:        number;
+  private _type:      string;
+  private _level:     number;
+  private _status:    string;
+  private _deadline:  number;
+  
+  get id():     number  { return this._id; }
+  get type():   string  { return this._type; }
+  get level():  number  { return this._level; }
+  get status(): string  { return this._status; }
 
   constructor() {
-    this.id       = ++Project._lastId;
-    this.type     = _.sample( Project._params.type );
-    this.level    = _.sample( Project._params.level );
-    this.status   = ProjectStatus.New;
-    this._deadline = this.level;
+    this._id       = ++Project._lastId;
+    this._type     = _.sample( Project._params.type );
+    this._level    = _.sample( Project._params.level );
+    this._status   = ProjectStatus.New;
+    this._deadline = this._level;
   }
 
   public testProject() {
-    this.status = ProjectStatus.Testing;
+    this._status = ProjectStatus.Testing;
     this._deadline = 1;
   }
 
   public completeProject() {
-    this.status = ProjectStatus.Completed;
+    this._status = ProjectStatus.Completed;
     this._deadline = 0;
   }
 
